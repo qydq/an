3d78270c56e42c8178e0edcffc62e8a7e0bbaab5
unknown <>
2016-09-10 21:51:52 +0800

    new lib2test

---
+++ an/an-zxing/src/main/java/com/an/an_zxing/AarNewTest.java
@@ -0,0 +1,15 @@
+package com.an.an_zxing;
+
+/**
+ * Created by stary on 2016/9/10.
+ * 文件名称：AarNewTest
+ * 文件描述：
+ * 作者：staryumou@163.com
+ * 修改时间：2016/9/10
+ */
+
+public class AarNewTest {
+    public String getNewEmail() {
+        return "qyddai@gmail.com";
+    }
+}
